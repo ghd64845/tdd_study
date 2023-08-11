6c627c7daaf9a15d7430a1f996c4f1abe4ab59c3
ghd64845 <fleuve1008@gmail.com>
2023-08-11 11:34:19 +0900

    chore: github action setting

---
+++ __test__/todolist/get-todolist.controller.e2e-test.js
@@ -0,0 +1,10 @@
+import { getServer } from "../lib/utils"
+
+describe("TodolistController e2e Test", () => {
+  const app = getServer()
+  const req = request(app)
+
+  const rootApiPath = "/api/todolist"
+
+  describe("", () => {})
+})
